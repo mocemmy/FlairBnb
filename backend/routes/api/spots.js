ed90e097a1fcd7047220af52d4ebb8b945a25d20
Emily Morgan <mocemmy@aol.com>
2023-06-13 16:10:06 -0400

    finished get-all-spots

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const { Op } = require('sequelize');
 const bcrypt = require('bcryptjs');
-const { check } = require('express-validator');
-const { handleValidationErrors } = require('../../utils/validation.js');
-const { setTokenCookie, restoreUser } = require('../../utils/auth');
-const { User, Review, SpotImage, sequelize, Sequelize } = require('../../db/models');
+const { User, Spot, Review, SpotImage, sequelize, Sequelize } = require('../../db/models');
 
 const router = express.Router();
 
-const { Spot } = require('../../db/models');
+
 
 //GET all spots:
 router.get('/', async(req, res) => {
