05125ca7aad7775dcc94b31938540b0eeb8d3390
Emily Morgan <mocemmy@aol.com>
2023-06-13 22:02:17 -0400

    added spots endpoints

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -2,8 +2,53 @@ const express = require('express');
 // const { Op } = require('sequelize');
 // const bcrypt = require('bcryptjs');
 const { Spot, Review, SpotImage, sequelize } = require('../../db/models');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
+
+const validateSpot = [
+    check('address')
+        .exists({ checkFalsey: true })
+        .notEmpty()
+        .withMessage("Street address is required"),
+    check('city')
+        .exists({ checkFalsey: true })
+        .notEmpty()
+        .withMessage("City is required"),
+    check('state')
+        .exists({ checkFalsey: true })
+        .notEmpty()
+        .withMessage("State is required"),
+    check('country')
+        .exists({ checkFalsey: true })
+        .notEmpty()
+        .withMessage("Country is required"),
+    check('lat')
+        .exists({ checkFalsey: true })
+        .notEmpty()
+        .isNumeric({min: -90, max: 90})
+        .withMessage("Latitude is not valid"),
+    check('lng')
+        .exists({ checkFalsey: true })
+        .notEmpty()
+        .isNumeric({min: -180, max: 180})
+        .withMessage("Longitude is not valid"),
+    check('name')
+        .exists({ checkFalsey: true })
+        .notEmpty()
+        .isLength({max: 50})
+        .withMessage("Name must be less than 50 characters"),
+    check('description')
+        .exists({ checkFalsey: true })
+        .notEmpty()
+        .withMessage("Description is required"),
+    check('price')
+        .exists({ checkFalsey: true })
+        .notEmpty()
+        .withMessage("Price per day is required"),
+    handleValidationErrors
+]
 
 
 
@@ -33,6 +78,43 @@ router.get('/', async(req, res) => {
         group: ['Spot.id']
     });
     res.json({spots});
+})
+
+//Get all spots owned by the current user:
+router.get('/current', async(req, res) => {
+    const { user } = req;
+    const spots = await Spot.findAll({
+        where: {
+            "ownerId": user.toJSON().id
+        }
+    })
+    res.json({spots});
+    
+})
+
+//get details of a spot from an id:
+router.get('/:spotId', async(req, res) => {
+    const spot = await Spot.findByPk(req.params.spotId);
+    if(!spot) {
+        res.statusCode = 404;
+        res.json({
+            "message": "Spot couldn't be found"
+        })
+    }
+    res.json(spot);
+})
+
+
+//Create a new spot:
+router.post('/', validateSpot, async(req, res) => {
+    const { user } = req;
+    const ownerId = user.toJSON().id;
+    const {address, city, state, country, lat, lng, name, description, price} = req.body;
+
+    const newSpot = await Spot.create({ownerId, address, city, state, country, lat, lng, name, description, price})
+
+    res.statusCode = 201;
+    res.json(newSpot);
 })
 
 
