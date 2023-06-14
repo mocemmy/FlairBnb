a71436f650ca241cd2f327fab889bc5c4c638a51
Emily Morgan <mocemmy@aol.com>
2023-06-14 16:48:47 -0400

    added group owner.id

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -143,7 +143,7 @@ router.get('/:spotId', async(req, res) => {
                 [sequelize.fn('AVG', sequelize.col('Reviews.stars')), 'avgStarRating'],
             ]
         },
-        group: ['Spot.id', 'SpotImages.id']
+        group: ['Spot.id', 'SpotImages.id', 'Owner.id']
     });
     if(!spot) {
         res.statusCode = 404;
