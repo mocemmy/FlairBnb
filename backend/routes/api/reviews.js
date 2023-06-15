b308e6a64f7a8357717cc44b82b83fc6f1c94e5d
Emily Morgan <mocemmy@aol.com>
2023-06-15 11:16:48 -0400

    finished delete a review

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -6,6 +6,19 @@ const { requireAuth } = require('../../utils/auth');
 
 const router = express.Router();
 
+const validateReview = [
+    check('review')
+        .exists({ checkFalsey: true })
+        .notEmpty()
+        .withMessage('Review text is required'),
+    check('stars')
+        .exists({ checkFalsey: true })
+        .notEmpty()
+        .isInt({min: 1, max: 5})
+        .withMessage("Stars must be an integer from 1 to 5"),
+    handleValidationErrors
+]
+
 //Get all Reviews of the Current User
 router.get('/current', requireAuth, async(req, res) => {
     const { user } = req;
@@ -52,6 +65,56 @@ router.get('/current', requireAuth, async(req, res) => {
     res.json({Reviews});
 })
 
+
+//Edit a Review
+router.put('/:reviewId', requireAuth, validateReview, async(req, res) => {
+    const { user } = req;
+    const reviewId = req.params.reviewId;
+    const rev = await Review.findByPk(reviewId);
+    const { review, stars } = req.body;
+    if(!rev){
+        res.statusCode = 404;
+        return res.json({
+            message: "Review couldn't be found"
+        })
+    }
+    if(user.id !== rev.userId){
+        res.statusCode = 403;
+        return res.json({
+            message: "Forbidden"
+        })
+    }
+
+    rev.review = review;
+    rev.stars = stars;
+    await rev.save();
+
+    res.json(rev)
+
+})
+
+//Delete a review
+router.delete('/:reviewId', requireAuth, async(req, res) => {
+    const { user } = req;
+    const review = await Review.findByPk(req.params.reviewId);
+    if(!review){
+        res.statusCode = 404;
+        return res.json({
+            message: "Review couldn't be found"
+        })
+    }
+    if(user.id !== review.userId){
+        res.statusCode = 403;
+        return res.json({
+            message: "Forbidden"
+        })
+    }
+
+    await review.destroy();
+    res.json({
+        message: "Successfully deleted"
+    })
+})
 
 
 module.exports = router;
