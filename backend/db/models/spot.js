02567a13f569ecf8501832f8dc5a30cfb26047a1
Emily Morgan <mocemmy@aol.com>
2023-06-15 10:33:37 -0400

    fixed aliasing issue

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -25,7 +25,6 @@ module.exports = (sequelize, DataTypes) => {
 
       Spot.hasMany(models.SpotImage, {
         foreignKey: 'spotId',
-        as: 'previewImage',
         onDelete: 'CASCADE',
         hooks: true
       })
