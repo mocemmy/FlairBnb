769e9a789d0c73be8bfd1111e7d572dcf8335c05
Emily Morgan <mocemmy@aol.com>
2023-06-30 19:11:04 -0400

    finished styling nav bar

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,26 +1,18 @@
 // frontend/src/components/Navigation/index.js
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
-import * as sessionActions from '../../store/session';
 import './Navigation.css';
 
 function Navigation({ isLoaded }){
   const sessionUser = useSelector(state => state.session.user);
-  const dispatch = useDispatch();
-
-  const logout = (e) => {
-    e.preventDefault();
-    dispatch(sessionActions.logout());
-  };
 
   let sessionLinks;
   if (sessionUser) {
     sessionLinks = (
       <li>
         <ProfileButton user={sessionUser} />
-        <button onClick={logout}>Log Out</button>
       </li>
     );
   } else {
@@ -33,10 +25,11 @@ function Navigation({ isLoaded }){
   }
 
   return (
-    <ul>
+    <ul className='nav-links-container'>
       <li>
-        <NavLink exact to="/">Home</NavLink>
+        <NavLink style={{textDecoration: 'none'}} exact to="/"><i class="fa-solid fa-house-chimney"></i>Home</NavLink>
       </li>
+      <h1>FlairBnB</h1>
       {isLoaded && sessionLinks}
     </ul>
   );
