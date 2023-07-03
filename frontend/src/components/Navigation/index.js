83686340ef1af22d353e4da172cccb23e2455117
Emily Morgan <mocemmy@aol.com>
2023-07-03 11:49:35 -0400

    styled navigation bar

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -4,6 +4,8 @@ import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
+import imageLogo from '../../images/F-logo.png';
+import name from '../../images/flairbnb-logo.png';
 
 function Navigation({ isLoaded }){
   const sessionUser = useSelector(state => state.session.user);
@@ -11,7 +13,7 @@ function Navigation({ isLoaded }){
   return (
     <ul>
       <li>
-        <NavLink exact to="/">Home</NavLink>
+        <NavLink exact to="/"><img src={imageLogo} alt="logo"/><img src={name} alt="flairbnb"/></NavLink>
       </li>
       {isLoaded && (
         <li>
