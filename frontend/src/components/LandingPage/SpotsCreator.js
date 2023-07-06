fa2cd03d7d49497b5c85f4529cbd91824513e19d
Emily Morgan <mocemmy@aol.com>
2023-07-06 18:33:59 -0400

    fixed extra zeroes issue

--- frontend/src/components/LandingPage/SpotsCreator.js
+++ frontend/src/components/LandingPage/SpotsCreator.js
@@ -1,46 +1,52 @@
-import {useHistory} from 'react-router-dom';
-import {useState} from 'react';
-import './tooltip.css';
-import StarRating from './StarRating';
+import { useHistory } from "react-router-dom";
+import { useState } from "react";
+import "./tooltip.css";
+import StarRating from "./StarRating";
 
-function SpotsCreator ({spot}) {
-    const history = useHistory();
-    const [showTip, setShowTip] = useState(false);
-    const displayTip = () => {
-        setShowTip(true)
-    }
-    const hideTip = () => {
-        setShowTip(false);
-    }
-    const hidden = "tooltip-container " + showTip? "tool-tip" : "hidden";
+function SpotsCreator({ spot }) {
+  const history = useHistory();
+  const [showTip, setShowTip] = useState(false);
+  const displayTip = () => {
+    setShowTip(true);
+  };
+  const hideTip = () => {
+    setShowTip(false);
+  };
+  const hidden = "tooltip-container " + showTip ? "tool-tip" : "hidden";
 
-    const onClick = (e) => {
-        history.push(`/spots/${spot.id}`)
-    }
-    const price = parseInt(spot.price).toFixed(2);
-    return (
-        <div key={spot.id} className='grid-item'
-        onClick={e => onClick(e)}
-        onMouseEnter={displayTip}
-        onMouseLeave={hideTip}
-        >
-            <div 
-            className="image-container"
-            style={{
-                backgroundImage: `url(${spot.previewImage})`,
-                backgroundSize: 'cover'
-            }}
-            >
-            {showTip && <p className={hidden}>{spot.name}</p>}
-            </div>
-            <ul className='spot-info-container'>
-                <div className='city-star-container'>
-            <li className='spot-location'>{spot.city}, {spot.state}</li>
-            <StarRating spot={spot} />
-                </div>
-            <li><span className="price">${price}</span> night</li>
-            </ul>
+  const onClick = (e) => {
+    history.push(`/spots/${spot.id}`);
+  };
+  const price = parseInt(spot.price).toFixed(2);
+  return (
+    <div
+      key={spot.id}
+      className="grid-item"
+      onClick={(e) => onClick(e)}
+      onMouseEnter={displayTip}
+      onMouseLeave={hideTip}
+    >
+      <div
+        className="image-container"
+        style={{
+          backgroundImage: `url(${spot.previewImage})`,
+          backgroundSize: "cover",
+        }}
+      >
+        {showTip && <p className={hidden}>{spot.name}</p>}
+      </div>
+      <ul className="spot-info">
+        <div className="city-star-container">
+          <li className="spot-location">
+            {spot.city}, {spot.state}
+          </li>
+          <StarRating spot={spot} />
         </div>
-    )
+        <li>
+          <span className="price">${price}</span> night
+        </li>
+      </ul>
+    </div>
+  );
 }
 export default SpotsCreator;
--- frontend/src/components/LandingPage/StarRating.js
+++ frontend/src/components/LandingPage/StarRating.js
@@ -1,8 +1,13 @@
 const StarRating = ({spot}) => {
+    let avgStars = "new";
+    if(spot.avgRating){
+        avgStars = parseInt(spot.avgRating).toFixed(2);
+    }
+    
 
     return (
         <>
-            {spot.avgRating && <p className="star-rating"><i className="fa-solid fa-star"></i>{spot.avgRating}</p>}
+            {spot.avgRating && <p className="star-rating"><i className="fa-solid fa-star"></i>{avgStars}</p>}
             {!spot.avgRating && <p>new</p>}
         </>
     )
